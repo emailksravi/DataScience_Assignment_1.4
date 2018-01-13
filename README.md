38231c9e118f6047086a9ce7f24529c2e02d8865
Ravishankar Suribabu <Ravishankar.Suribabu@walmart.com>
2018-01-13 18:16:56 +0530

    Code check-in for practice

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# DataScience_Assignment_1.4
+# Problem Statement :
+```
+Write a Python program to find the volume of a sphere with diameter 12 cm.
+Formula: V=4/3 * π * r^3
+```
+
+## Snapshot - Run from jupyter REPL 
+```
+import get_sphere_volume.ipynb in jupyter REPL 
+```
+
+![alt text](screenshots/get_sphere_volume_jupyter_repl.png "Run from jupyter repl")
+
+
+## Snapshot - Run from command line bash
+```
+Command to execute : python get_sphere_volume.py
+```
+
+![alt text](screenshots/get_sphere_volume_command_line_bash.png "Run from command line bash")
---
+++ get_sphere_volume.py
@@ -0,0 +1,20 @@
+"""
+Problem Statement :
+Write a Python program to find the volume of a sphere with diameter 12 cm.
+Formula: V=4/3 * π * r^3
+"""
+
+import math
+
+# Function to calculate volume of sphere - given input diameter
+# round off to 3 decimal places
+def calc_sphere_volume(diameter):
+    volume = round(4 / 3 * math.pi * (diameter/2) ** 3, 3 )
+    return volume
+
+# init diameter
+diameter = 12
+
+# print final volume of sphere for given diameter
+print ("Volume of sphere for diameter 12 cms : ", calc_sphere_volume(diameter), " cubic centimetre")
+
